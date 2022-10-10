626e7b9b281a15db7324cee7d0dfb3fe52e458c3
Silksongs <71660204+Silksongs@users.noreply.github.com>
2022-10-10 22:52:32 +0300

    Work_5
    Work_5

---
+++ work_5/src/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {Block} from './Block';
+import './index.scss';
+
+function App() {
+    //const [rates, setRats] = React.useState({});
+    const ratesRef = React.useRef({})
+
+    const [fromCurrency, setFromCurrency] = React.useState('RUB');
+    const [toCurrency, setToCurrency] = React.useState('USD');
+    const [fromPrice, setFromPrice] = React.useState(0);
+    const [toPrice, setToPrice] = React.useState(1);
+
+    React.useEffect(() => {
+        fetch('https://cdn.cur.su/api/latest.json')
+            .then((res) => res.json())
+            .then(json => {
+                //setRats(json.rates)
+                ratesRef.current = json.rates
+                onChangeToPrice(1)
+
+            }).catch(err => {
+            console.warn(err)
+            alert('Не удалось получить информацию');
+        });
+    }, []);
+
+    const onChangeFromPrice = (value) => {
+        const price = value / ratesRef.current[fromCurrency]
+        const result = price * ratesRef.current[toCurrency]
+        setFromPrice(result.toFixed(3))
+        setToPrice(value)
+    }
+    const onChangeToPrice = (value) => {
+        const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value
+        setFromPrice(result.toFixed(3))
+        setToPrice(value)
+    }
+
+    React.useEffect(() => {
+        onChangeFromPrice(fromPrice)
+    }, [fromCurrency])
+
+    React.useEffect(() => {
+        onChangeToPrice(toPrice)
+    }, [toCurrency])
+
+
+    return (
+        <div className="App">
+            <Block value={fromPrice} currency={fromCurrency} onChangeCurrency={setFromCurrency}
+                   onChangeValue={onChangeFromPrice}/>
+            <Block value={toPrice} currency={toCurrency} onChangeCurrency={setToCurrency}
+                   onChangeValue={onChangeToPrice}/>
+        </div>
+    );
+}
+
+export default App;
